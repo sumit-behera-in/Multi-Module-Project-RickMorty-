b094e6930cf3bcebd13afbd9501cbdce232812da
sumitbehera1508 <100491275+sumitbehera1508@users.noreply.github.com>
2024-05-15 02:10:14 +0530

    init

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Rick and Morty Multi Module App"
+include(":app")
+include(":network")
